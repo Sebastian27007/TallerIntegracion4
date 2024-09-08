0f5e2da5865338be680d7871731c49d89cdd80ef
Souley <souleynu@gmail.com>
2024-09-07 22:55:34 -0400

    se agrega el logo de la app en el apartado de inicio y se crea la ventana de registro

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar'; //barra superior de hora y bateria, se utiliza con estilo automatico (black/white)
 import React from 'react'; //por default hay que tenerlo
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'; //nos permite crear la función y guardarla en la variable styles
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native'; //nos permite crear la función y guardarla en la variable styles
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import RecuperarClave from './ventanas/RecuperarClave';
+import CrearCuenta from './ventanas/CrearCuenta';
 
 const Stack = createStackNavigator();
 
 function Inicio({ navigation }) {
   return (
     <View style={styles.container}>
+      <Image source={require('./logo.png')} style={styles.logo} />
       <Text style={styles.titulo}>Bienvenido</Text>
       <Text style={styles.subtitulo}>Accede a tu cuenta</Text>
       <TextInput 
@@ -24,7 +26,9 @@ function Inicio({ navigation }) {
       <TouchableOpacity onPress={() => navigation.navigate('RecuperarClave')}>
         <Text style={styles.olvidaste_contraseña}>Olvidaste tu contraseña</Text>
       </TouchableOpacity>
-      <Text style={styles.olvidaste_contraseña}>¿No tienes una cuenta?</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('CrearCuenta')}>
+        <Text style={styles.olvidaste_contraseña}>¿No tienes una cuenta?</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" /> 
       <TouchableOpacity style={styles.boton}>
         <Text style={{fontSize: 17, fontWeight: '400', color: 'grey', fontFamily: 'System',}}>Ingresar</Text>
@@ -39,6 +43,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Inicio} options={{ headerShown: false }}/>
         <Stack.Screen name="RecuperarClave" component={RecuperarClave} options={{ headerShown: false }}/>
+        <Stack.Screen name='CrearCuenta' component={CrearCuenta} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -50,6 +55,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#f1f1f1',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 100, 
+    height: 100,
+    marginBottom: 20, // Espacio entre el logo y el título
   },
   titulo: {
     fontSize: 55,
